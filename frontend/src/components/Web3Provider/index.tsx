d5bdcb7aa3d36085d830fd5b2622a0abca825e56
MichaelScofield <michaelscofieldafs@gmail.com>
2025-12-18 14:51:20 -0300

    Adjusts
    Adjusts

--- frontend/src/components/Web3Provider/index.tsx
+++ frontend/src/components/Web3Provider/index.tsx
@@ -49,7 +49,7 @@ export function Web3Provider({ children }: { children: ReactNode }) {
             '--w3m-qr-color': '#99E39E',
         },
         enableReconnect: true,
-        networks: [sonicTestnet, baseSepolia],
+        networks: [sonicTestnet],
         chainImages: {
             146: 'https://resources.cryptocompare.com/asset-management/17157/1727687183179.png',
             64_165: 'https://resources.cryptocompare.com/asset-management/17157/1727687183179.png',
@@ -74,6 +74,6 @@ export function Web3Provider({ children }: { children: ReactNode }) {
 }
 
 export const wagmiAdapter = new WagmiAdapter({
-    networks: [sonicTestnet, baseSepolia],
+    networks: [sonicTestnet],
     projectId,
 })
